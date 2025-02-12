21c9a612f3a4757aa484890fab2842bc8205c994
Elwin BASSAGET <elwin.bassaget@data-tricks.fr>
2025-02-12 16:51:26 +0100

    add more tests

--- chess.test.js
+++ chess.test.js
@@ -1,6 +1,11 @@
 const getNeighbourCases = require('./index.js');
 const solveNQueens = require('./index.js');
 
+
+test('get chessboard from n1', () => {
+    expect(solveNQueens(1)).toEqual([["#"]]);
+});
+
 test('get chessboard from n4', () => {
     const solutions = solveNQueens(4);
     expect(solutions).toContainEqual(["O#OO", "OOO#", "#OOO", "OO#O"], ["OO#O", "#OOO", "OOO#", "O#OO"]);
@@ -8,6 +13,13 @@ test('get chessboard from n4', () => {
 
 test('get chessboard from n2', () => {
     const solutions = solveNQueens(2);
-    expect(solutions).toContainEqual([" "]);
+    expect(solutions).toContainEqual([]);
 });
 
+test('solutions for negative n', () => {
+    expect(solveNQueens(-1)).toEqual([]);
+});
+
+test('solutions for non-integer n', () => {
+    expect(solveNQueens(2.5)).toEqual([]);
+});
